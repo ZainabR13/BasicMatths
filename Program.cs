e09a008d36885ab3b6ce830cac2abcb94cde954e
zainabrauf133@gmail.com <zainabrauf133@gmail.com>
2022-11-14 20:47:09 -0800

    Changes has been made by me,

--- Program.cs
+++ Program.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace BasicMatths
 {
-    class Program
+    public class BasicMathsOperation
     {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Substract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            // To trace error while testing, writing + operator instead of * operator.  
+            return num1 + num2;
+        }
         static void Main(string[] args)
         {
         }
